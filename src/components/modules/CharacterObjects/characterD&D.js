b574b12c27659f6c68be8bdf2b69aa1ccb253363
ednelson89 <ednelson89@gmail.com>
2019-12-01 23:16:33 -0500

    D&D5e Character View Added

--- src/components/modules/CharacterObjects/characterD&D.js
+++ src/components/modules/CharacterObjects/characterD&D.js
@@ -51,7 +51,83 @@ const charDnD5e = () => ({
       mod: 0,
       save: 0
     }
-  ]
+  ],
+  combatStats: {
+    hitPoints: {
+      hitDice: 0,
+      maxHP: 0,
+      currHP: 0,
+      tempHP: 0
+    },
+    deathSaves: {
+      success: 0,
+      fails: 0
+    },
+    armorClass: 0,
+    initiative: 0,
+    inspiration: false,
+    defenses: [""],
+    proficiencyBonus: 0,
+    speed: 0
+  },
+  skillsList: [
+    { skName: "Acrobatics", skill: "Dex", prof: false, mod: 0 },
+    { skName: "Animal Handling", skill: "Wis", prof: false, mod: 0 },
+    { skName: "Arcana", skill: "Int", prof: false, mod: 0 },
+    { skName: "Athletics", skill: "Str", prof: false, mod: 0 },
+    { skName: "Deception", skill: "Cha", prof: false, mod: 0 },
+    { skName: "History", skill: "Int", prof: false, mod: 0 },
+    { skName: "Insight", skill: "Wis", prof: false, mod: 0 },
+    { skName: "Intimidation", skill: "Cha", prof: false, mod: 0 },
+    { skName: "Investigation", skill: "Int", prof: false, mod: 0 },
+    { skName: "Medicine", skill: "Wis", prof: false, mod: 0 },
+    { skName: "Nature", skill: "Int", prof: false, mod: 0 },
+    { skName: "Perception", skill: "Wis", prof: false, mod: 0 },
+    { skName: "Performance", skill: "Cha", prof: false, mod: 0 },
+    { skName: "Persuasion", skill: "Cha", prof: false, mod: 0 },
+    { skName: "Religion", skill: "Int", prof: false, mod: 0 },
+    { skName: "Sleight of Hand", skill: "Dex", prof: false, mod: 0 },
+    { skName: "Stealth", skill: "Dex", prof: false, mod: 0 },
+    { skName: "Survival", skill: "Wis", prof: false, mod: 0 }
+  ],
+  proficiencies: [""],
+  languages: [""],
+  passiveScores: {
+    passPercep: 0,
+    passInsight: 0,
+    passInvest: 0
+  },
+  attacksCantrips: [{ attackName: "", hitMod: 0, dmg: "", notes: "" }],
+  equipment: [{ itemName: "", qty: 0, weight: 0 }],
+  features: [""],
+  money: [
+    {
+      copper: 0,
+      silver: 0,
+      gold: 0,
+      electrum: 0,
+      platinum: 0
+    }
+  ],
+  backstory: [""],
+  note: [""],
+  spells: {
+    cantrips: [{ spellName: "", spellDescription: "" }],
+    lvl1: [{ spellName: "", spellDescription: "" }],
+    lvl2: [{ spellName: "", spellDescription: "" }],
+    lvl3: [{ spellName: "", spellDescription: "" }],
+    lvl4: [{ spellName: "", spellDescription: "" }],
+    lvl5: [{ spellName: "", spellDescription: "" }],
+    lvl6: [{ spellName: "", spellDescription: "" }],
+    lvl7: [{ spellName: "", spellDescription: "" }],
+    lvl8: [{ spellName: "", spellDescription: "" }],
+    lvl9: [{ spellName: "", spellDescription: "" }]
+  },
+  spellMods: {
+    spellAbility: "",
+    spellSave: 0,
+    spellAttack: 0
+  }
 });
 
 var generateID = function() {
--- src/components/views/CampaignModules/CharacterViews/viewModules/ViewDnD5e.vue
+++ src/components/views/CampaignModules/CharacterViews/viewModules/ViewDnD5e.vue
@@ -69,32 +69,297 @@
       </b-col>
     </b-row>
     <hr />
-    <!-- Stat Block -->
-    <b-row>
-      <b-col
-        class="skillCols"
-        cols="2"
-        v-for="(stat, index) in char.statsBlock"
-        :key="index"
-        :style="{'background-image': 'url(' + require('@/assets/statCard.jpg') + ')'}"
-      >
-        <p class="skillLabel">{{stat.statName}}</p>
-        <p class="score">{{stat.stat}}</p>
-        <p class="skillLabel">Mod:</p>
-        <p class="score">{{stat.mod}}</p>
-        <p class="skillLabel">Save:</p>
-        <p class="score">{{stat.save}}</p>
-      </b-col>
-    </b-row>
-    <hr />
-    <!-- Skills Block -->
+    <b-tabs content-class="mt-3">
+      <b-tab title="Core Stats" active>
+        <!-- Combat Info Block -->
+        <b-row>
+          <b-col>
+            <b-card class="b-cards">
+              <b-row>
+                <b-col cols="3" style="border-right:1px solid #000;">
+                  <p class="sectionLabel">Hit Points</p>
+                  <p class="score">
+                    <span class="skillLabel">Current:</span>
+                    {{char.combatStats.hitPoints.currHP}}
+                  </p>
+                  <p class="score">
+                    <span class="skillLabel">Max:</span>
+                    {{char.combatStats.hitPoints.maxHP}}
+                  </p>
+                  <p class="score">
+                    <span class="skillLabel">Temporary:</span>
+                    {{char.combatStats.hitPoints.tempHP}}
+                  </p>
+                  <p class="score">
+                    <span class="skillLabel">Hit Dice:</span>
+                    {{char.combatStats.hitPoints.hitDice}}
+                  </p>
+                </b-col>
+                <b-col cols="3" style="border-right:1px solid #000;">
+                  <p class="skillLabel">Armor Class:</p>
+                  <p class="score">{{char.combatStats.armorClass}}</p>
+                  <p class="skillLabel">Initiative:</p>
+                  <p class="score">{{char.combatStats.initiative}}</p>
+                  <p class="skillLabel">Speed:</p>
+                  <p class="score">{{char.combatStats.speed}}</p>
+                </b-col>
+                <b-col cols="3" style="border-right:1px solid #000;">
+                  <p class="skillLabel">Proficiency Bonus:</p>
+                  <p class="score">{{char.combatStats.proficiencyBonus}}</p>
+                  <p class="skillLabel">Inspiration:</p>
+                  <p class="score">{{char.combatStats.inspiration ? "Yes" : "No"}}</p>
+                  <p class="skillLabel">Death Saves:</p>
+                  <p class="score">Succeed: {{ char.combatStats.deathSaves.success }}</p>
+                  <p class="score">Failed: {{ char.combatStats.deathSaves.fails }}</p>
+                </b-col>
+                <b-col cols="3">
+                  <p class="sectionLabel">Passive Skills</p>
+                  <p class="skillLabel">Passive Perception:</p>
+                  <p class="score">{{char.passiveScores.passPercep}}</p>
+                  <p class="skillLabel">Passive Insight:</p>
+                  <p class="score">{{char.passiveScores.passInsight}}</p>
+                  <p class="skillLabel">Passive Investigation:</p>
+                  <p class="score">{{char.passiveScores.passInvest}}</p>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+        </b-row>
+        <hr />
+        <!-- Stat Block -->
+        <b-row>
+          <b-col>
+            <b-card class="b-cards">
+              <b-row>
+                <b-col
+                  class="skillCols"
+                  cols="2"
+                  v-for="(stat, index) in char.statsBlock"
+                  :key="index"
+                  :style="{'background-image': 'url(' + require('@/assets/statCard.jpg') + ')'}"
+                >
+                  <p class="skillLabel">{{stat.statName}}</p>
+                  <p class="score">{{stat.stat}}</p>
+                  <p class="skillLabel">Mod:</p>
+                  <p class="score">{{stat.mod}}</p>
+                  <p class="skillLabel">Save:</p>
+                  <p class="score">{{stat.save}}</p>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+        </b-row>
+        <hr />
+        <!-- Attacks -->
+        <b-row>
+          <b-col>
+            <b-card class="b-cards">
+              <b-row>
+                <b-col>
+                  <h4 style="font-weight:bold;">Attacks and Cantrips:</h4>
+                  <b-table striped hover :items="char.attacksCantrips"></b-table>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-tab>
+      <b-tab :title="'Skills & Proficiencies'">
+        <!-- Skills Block -->
+        <b-row>
+          <b-col>
+            <b-card class="b-cards">
+              <b-row>
+                <b-col>
+                  <h4
+                    style="font-weight: bold;"
+                    v-b-toggle="'skillsCollapse'"
+                    @click="skillsOpen = !skillsOpen"
+                  >
+                    Skills:
+                    <v-icon v-if="!skillsOpen" role="tab">mdi-chevron-double-right</v-icon>
+                    <v-icon v-else-if="skillsOpen">mdi-chevron-double-down</v-icon>
+                    <span
+                      class="italics"
+                      style="font-weight:normal;font-size: 12px;"
+                      v-if="!skillsOpen"
+                    >(click to expand)</span>
+                  </h4>
+                </b-col>
+              </b-row>
+              <b-collapse id="skillsCollapse">
+                <b-row>
+                  <b-col
+                    cols="2"
+                    v-for="(skill, index) in char.skillsList"
+                    :key="index"
+                    style="border-right:1px solid #000;"
+                  >
+                    <p class="sectionLabel">{{skill.skName}}</p>
+                    <p class="score">{{skill.mod}}</p>
+                    <p class="skillLabel">Skill Stat:</p>
+                    <p class="score">{{skill.skill}}</p>
+                    <p class="skillLabel">Proficient:</p>
+                    <p class="score">{{skill.proficient ? "Yes" : "No"}}</p>
+                  </b-col>
+                </b-row>
+              </b-collapse>
+            </b-card>
+          </b-col>
+        </b-row>
+        <hr />
+        <!--  Proficiencies and Lanuages  -->
+        <b-row>
+          <b-col>
+            <b-card class="b-cards">
+              <b-row>
+                <b-col cols="6" style="border-right:1px solid #000;">
+                  <p class="sectionLabel">Proficiencies</p>
+                  <p v-for="(prof, index) in char.proficiencies" :key="index">{{prof}}</p>
+                </b-col>
+                <b-col cols="6">
+                  <p class="sectionLabel">Languages</p>
+                  <p v-for="(lang, index) in char.languages" :key="index">{{lang}}</p>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+        </b-row>
+        <hr />
+      </b-tab>
+      <b-tab :title="'Features & Equipment'">
+        <!-- Features -->
+        <b-row>
+          <b-col>
+            <b-card class="b-cards">
+              <b-row>
+                <b-col>
+                  <h4 style="font-weight:bold;">Features:</h4>
+                  <p v-for="(feature, index) in char.features" :key="index">{{feature}}</p>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+        </b-row>
+        <hr />
+        <!-- Equipment  -->
+        <b-row>
+          <b-col>
+            <b-card class="b-cards">
+              <b-row>
+                <b-col>
+                  <h4 style="font-weight:bold;">Equipment:</h4>
+                  <b-table striped hover :items="char.equipment"></b-table>
+                </b-col>
+              </b-row>
+              <b-row>
+                <b-col>
+                  <h4 style="font-weight:bold;">Money:</h4>
+                  <b-table striped hover :items="char.money"></b-table>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-tab>
+      <b-tab :title="'Biography & Notes'">
+        <!-- Biography -->
+        <b-row>
+          <b-col>
+            <b-card class="b-cards">
+              <b-row>
+                <b-col>
+                  <h4 style="font-weight:bold;">Character Backstory:</h4>
+                  <p v-for="(story, index) in char.backstory" :key="index">{{story}}</p>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+        </b-row>
+        <hr />
+        <!-- Additional Notes -->
+        <b-row>
+          <b-col>
+            <b-card class="b-cards">
+              <b-row>
+                <b-col>
+                  <h4 style="font-weight:bold;">Additional Notes:</h4>
+                  <p v-for="(note, index) in char.notes" :key="index">{{note}}</p>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-tab>
+      <b-tab :title="'Spells'">
+        <b-row>
+          <b-col>
+            <b-card class="b-cards">
+              <b-row>
+                <b-col>
+                  <h4 style="font-weight:bold;">Spell Modifiers:</h4>
+                </b-col>
+              </b-row>
+              <b-row>
+                <b-col cols="4" style="border-right:1px solid #000;">
+                  <p class="skillLabel">Spell Ability:</p>
+                  <p class="score">{{char.spellMods.spellAbility}}</p>
+                </b-col>
+                <b-col cols="4" style="border-right:1px solid #000;">
+                  <p class="skillLabel">Spell Save DC:</p>
+                  <p class="score">{{char.spellMods.spellSave}}</p>
+                </b-col>
+                <b-col cols="4">
+                  <p class="skillLabel">Spell Attack Bonus:</p>
+                  <p class="score">{{char.spellMods.spellAttack}}</p>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+        </b-row>
+        <hr />
+        <b-row>
+          <b-col>
+            <b-card class="b-cards">
+              <b-row>
+                <b-col>
+                  <h4 style="font-weight:bold;">Spells:</h4>
+                  <p class="sectionLabel">Cantrips:</p>
+                  <b-table striped hover :items="char.spells.cantrips"></b-table>
+                  <p class="sectionLabel">Level 1:</p>
+                  <b-table striped hover :items="char.spells.lvl1"></b-table>
+                  <p class="sectionLabel">Level 2:</p>
+                  <b-table striped hover :items="char.spells.lvl2"></b-table>
+                  <p class="sectionLabel">Level 3:</p>
+                  <b-table striped hover :items="char.spells.lvl3"></b-table>
+                  <p class="sectionLabel">Level 4:</p>
+                  <b-table striped hover :items="char.spells.lvl4"></b-table>
+                  <p class="sectionLabel">Level 5:</p>
+                  <b-table striped hover :items="char.spells.lvl5"></b-table>
+                  <p class="sectionLabel">Level 6:</p>
+                  <b-table striped hover :items="char.spells.lvl6"></b-table>
+                  <p class="sectionLabel">Level 7:</p>
+                  <b-table striped hover :items="char.spells.lvl7"></b-table>
+                  <p class="sectionLabel">Level 8:</p>
+                  <b-table striped hover :items="char.spells.lvl8"></b-table>
+                  <p class="sectionLabel">Level 9:</p>
+                  <b-table striped hover :items="char.spells.lvl9"></b-table>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-tab>
+    </b-tabs>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      skillsOpen: false
+    };
   },
   computed: {
     char() {
@@ -111,6 +376,11 @@ p {
 }
 .charImg {
   width: 175px;
+}
+.sectionLabel {
+  text-align: center !important;
+  font-weight: bold;
+  text-decoration: underline;
 }
 .skillLabel {
   text-align: center !important;
