9fe8baaf05e619193c5b5fb6e2782f9c3558e14d
ednelson89 <ednelson89@gmail.com>
2019-12-16 00:01:18 -0500

    overrode routers push function to resturn a promise

--- src/router/index.js
+++ src/router/index.js
@@ -84,6 +84,13 @@ const routes = [
   }
 ];
 
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject)
+    return originalPush.call(this, location, onResolve, onReject);
+  return originalPush.call(this, location).catch(err => err);
+};
+
 const router = new VueRouter({
   scrollBehavior() {
     return { x: 0, y: 0 };
