2abc40c2e61db2ea3165456e201a9186b87ac8a4
ednelson89 <ednelson89@gmail.com>
2020-02-07 16:25:19 -0500

    added tutorial playlist

--- src/components/views/ResourcesModules/dndResources.vue
+++ src/components/views/ResourcesModules/dndResources.vue
@@ -1,17 +1,31 @@
 <template>
   <div>
     <b-row>
-      <b-col cols="6">
+      <b-col cols="4">
         <h3>Source Books</h3>
         <p v-for="(resource, index) in dnd5e.sourceBooks" :key="index">
           <a :href="resource.link" target="_blank">{{resource.title}}</a>
         </p>
       </b-col>
-      <b-col cols="6">
+      <b-col cols="8">
         <h3>Supplemental Materials</h3>
         <p v-for="(resource, index) in dnd5e.supplemental" :key="index">
           <a :href="resource.link" target="_blank">{{resource.title}}</a>
         </p>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <h3>Handbooker Helper Tutorials</h3>
+        <iframe
+          style="margin: 0 auto"
+          width="640"
+          height="390"
+          src="https://www.youtube.com/embed/videoseries?list=PL1tiwbzkOjQyr6-gqJ8r29j_rJkR49uDN"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
       </b-col>
     </b-row>
   </div>
